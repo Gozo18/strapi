6269f02792bb46fd84efffaf82bac62c1038af0b
Roman Jelinek <jelinek82@gmail.com>
2025-09-23 10:21:18 +0200

    Sendgrid to Resend

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,33 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  },
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "info@vinoiris.cz",
+        defaultReplyTo: "info@vinoiris.cz",
       },
     },
-    email: {
-      config: {
-        provider: "sendgrid",
-        providerOptions: {
-          apiKey: env("SENDGRID_API_KEY"),
-        },
-        settings: {
-          defaultFrom: "info@irispavlov.cz",
-          defaultReplyTo: "info@irispavlov.cz",
-        },
-      },
-    },
-  });
+  },
+});
